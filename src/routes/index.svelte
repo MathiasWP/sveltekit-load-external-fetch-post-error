1cd93d970e9993d62e0e5ca495713fa14c785ff2
Mathias Picker <mattis123@live.no>
2022-07-07 16:13:58 +0200

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,17 @@
+<script context="module" >
+	export const load = async function ({ fetch }) {
+		await fetch('https://example.com', {
+			method: 'POST',
+			body: JSON.stringify({
+				foo: 'bar'
+			}),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+
+        return {}
+	};
+</script>
+
+Hi!
